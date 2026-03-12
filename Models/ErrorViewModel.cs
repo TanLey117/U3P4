6c45f2cfc223e899daaca1f70e56d6ca97a6f064
TanLey117 <22340372@itnogales.edu.mx>
2026-03-11 21:24:19 -0700

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace U3P4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
