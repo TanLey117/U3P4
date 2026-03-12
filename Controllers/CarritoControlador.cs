9c586569971a375be68433cd20e4092f3600ce4a
TanLey117 <22340372@itnogales.edu.mx>
2026-03-11 21:31:04 -0700

    Creacion del controlador CarritoControlador

---
+++ Controllers/CarritoControlador.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U3P4.Models;
+
+namespace U3P4.Controllers
+{
+    public class CarritoControlador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
