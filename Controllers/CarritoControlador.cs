842ae3a9f8c8d14c582365aa623dec10ec5b53fd
TanLey117 <22340372@itnogales.edu.mx>
2026-03-11 21:40:16 -0700

    Completacion del controlador CarritoControlador

--- Controllers/CarritoControlador.cs
+++ Controllers/CarritoControlador.cs
@@ -5,9 +5,40 @@ namespace U3P4.Controllers
 {
     public class CarritoControlador : Controller
     {
+        static List<Producto> productos = new List<Producto>();
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Producto p)
+        {
+            p.Id = productos.Count + 1;
+            productos.Add(p);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var producto = productos.FirstOrDefault(x => x.Id == id);
+            return View(producto);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Producto p)
+        {
+            var producto = productos.FirstOrDefault(x => x.Id == p.Id);
+
+            if (producto != null)
+                productos.Remove(producto);
+
+            return RedirectToAction("Index");
         }
     }
 }
