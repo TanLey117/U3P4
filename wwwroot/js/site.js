ce3ba9c5739e112924b73df43c996876abfc7557
TanLey117 <22340372@itnogales.edu.mx>
2026-03-18 18:34:52 -0700

    Add: se agrega la funcion para guardar en SessionStorage

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Guardar en SessionStorage
+function guardarProducto(nombre, precio) {
+
+    let productos = JSON.parse(sessionStorage.getItem("productos")) || [];
+
+    productos.push({
+        nombre: nombre,
+        precio: precio
+    });
+
+    sessionStorage.setItem("productos", JSON.stringify(productos));
+}
+
+// Mostrar en consola (para evidencia)
+function mostrarProductos() {
+    let productos = JSON.parse(sessionStorage.getItem("productos"));
+
+    console.log("Productos en SessionStorage:", productos);
+} 
